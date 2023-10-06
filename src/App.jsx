73be599f28f90f13435b076b10b5ed1b066230be
santhosh raghav <santhoshraghav.work@gmail.com>
2023-10-06 16:26:49 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './features/counterSlice';
 
 function App() {
-  return <>hi</>;
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.count);
+  const [isValueChanged, setIsValueChanged] = useState(false);
+
+  useEffect(() => {
+    setIsValueChanged(true);
+    const timeoutId = setTimeout(() => {
+      setIsValueChanged(false);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [count]);
+
+  return (
+    <>
+      <p className={isValueChanged ? 'pluse' : 'value'}>{count}</p>
+      <div className='actions'>
+        <button className='btn' onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button className='btn' onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
